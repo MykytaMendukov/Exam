fb744ac2a48a7d7db395ab8fcf54e6ff1dbdfa65
Мєндюков Микита Романович <131793451+MykytaMendukov@users.noreply.github.com>
2024-03-28 21:30:52 +0200

    Exam.cpp
    Ex 2

--- Exam/Exam.cpp
+++ Exam/Exam.cpp
@@ -1,20 +1,27 @@
-// Exam.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    int age;
+    try {
+        cout << "Enter your age: ";
+        cin >> age;
+        if (age < 18 && age > 0) {
+            cout << "You are younger 18!" << endl;
+        }
+        else if(age >= 18){
+            cout << "Your are under or 18." << endl;
+        }
+        else{
+            throw -1;
+        }
+    }
+    catch (int err) {
+        cout << "We have some troubles!" << endl;
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
